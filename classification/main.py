8136402ec2d9d3eb35095bf5691d928b7b8d10ba
onebottlekick <gksqudcks97@gmail.com>
2022-04-18 18:06:26 +0900

    loss plot

--- classification/main.py
+++ classification/main.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader, random_split
 
 from dataset import HealthData
 from models import FeedForward
-from utils import evaluate, train
+from utils import evaluate, train, plot_loss
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 BATCH_SIZE = 32
@@ -22,10 +22,16 @@ model = FeedForward().to(DEVICE)
 criterion = nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters())
 
+train_losses = []
+val_losses = []
 for epoch in range(NUM_EPOCHS):
     train_loss = train(model, train_loader, optimizer, criterion, epoch, DEVICE)
+    train_losses.append(train_loss)
     val_loss = evaluate(model, val_loader, criterion, DEVICE)
+    val_losses.append(val_loss)
     
     print(f'Epoch {epoch+1:02}')
     print(f'\tTrain Loss: {train_loss:.3f}')
     print(f'\tVal Loss: {val_loss:.3f}')
+    
+plot_loss(train_losses, val_losses)
