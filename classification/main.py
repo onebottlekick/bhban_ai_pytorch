bb158d8229e118e5639c3295ebbf9589700a7e90
onebottlekick <gksqudcks97@gmail.com>
2022-04-19 10:21:55 +0900

    implemented early stopping

--- classification/main.py
+++ classification/main.py
@@ -4,12 +4,12 @@ from torch.utils.data import DataLoader, random_split
 
 from dataset import HealthData
 from models import FeedForward
-from utils import evaluate, train, plot_loss
+from utils import evaluate, train, plot_loss, EarlyStopping
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 BATCH_SIZE = 32
 TEST_RATIO = 0.8
-NUM_EPOCHS = 20
+NUM_EPOCHS = 1000
 
 
 dataset = HealthData()
@@ -22,10 +22,12 @@ model = FeedForward().to(DEVICE)
 criterion = nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters())
 
+early_stopping = EarlyStopping(verbose=True)
+
 train_losses = []
 val_losses = []
 for epoch in range(NUM_EPOCHS):
-    train_loss = train(model, train_loader, optimizer, criterion, epoch, DEVICE)
+    train_loss = train(model, train_loader, optimizer, criterion, DEVICE)
     train_losses.append(train_loss)
     val_loss = evaluate(model, val_loader, criterion, DEVICE)
     val_losses.append(val_loss)
@@ -33,5 +35,10 @@ for epoch in range(NUM_EPOCHS):
     print(f'Epoch {epoch+1:02}')
     print(f'\tTrain Loss: {train_loss:.3f}')
     print(f'\tVal Loss: {val_loss:.3f}')
+    
+    early_stopping(val_loss, model)
+    if early_stopping.early_stop:
+        print('Early stopping')
+        break
     
 plot_loss(train_losses, val_losses)
--- classification/utils.py
+++ classification/utils.py
@@ -1,8 +1,11 @@
+import os
+
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
 
 
-def train(model, train_loader, optimizer, criterion, epoch, device):
+def train(model, train_loader, optimizer, criterion, device):
     model.train()
     train_epoch_loss = 0
     for idx, (inputs, targets) in enumerate(train_loader):
@@ -46,3 +49,42 @@ def plot_loss(train_losses, val_losses):
     plt.plot(val_losses, 'blue', label='val')
     plt.legend()
     plt.show()
+    
+    
+class EarlyStopping:
+    def __init__(self, patience=10, verbose=False, delta=0, save_path='checkpoints/cp.pt'):
+        self.patience = patience
+        self.verbose = verbose
+        self.delta = delta
+        self.save_path = save_path
+        
+        self.counter = 0
+        self.best_score = None
+        self.val_loss = np.Inf
+        self.early_stop = False
+        
+    def __call__(self, val_loss, model):
+        score = -val_loss
+        
+        if self.best_score is None:
+            self.best_score = score
+            self._save_model(val_loss, model)
+        
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EearlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+                
+        else:
+            self.best_score = score
+            self._save_model(val_loss, model)
+            self.counter = 0
+            
+    def _save_model(self, val_loss, model):
+        os.makedirs(self.save_path.split('/')[0], exist_ok=True)
+        if self.verbose:
+            print(f'Val loss decreased ({self.val_loss:.6f} --> {val_loss:.6f}).')
+            print('Saved model.')
+        torch.save(model.state_dict(), self.save_path)
+        self.val_loss = val_loss
