1a957c16ffe43ab25de420a43d251bb2a54da53f
onebottlekick <gksqudcks97@gmail.com>
2022-05-09 16:55:59 +0900

    weather prediction
    - dataset

---
+++ RNN/weather_prediction/dataset.py
@@ -0,0 +1,37 @@
+from torch.utils.data import Dataset
+import pandas as pd
+import os
+import torch
+
+
+class WeatherDataset(Dataset):
+    def __init__(self, root='data', normalize=False, window_size=12):
+        self.root = root
+        self.dataset_path = os.path.join(root, 'jena_climate_2009_2016.csv')
+        self.normalize = normalize
+        self.window_size = window_size
+        
+        self.inputs, self.targets = self._get_data()
+        
+    def _get_data(self):
+        dataset = pd.read_csv(self.dataset_path).drop('Date Time', axis=1).to_numpy()
+        
+        if self.normalize:
+            dataset = (dataset - dataset.min(axis=0))/(dataset.max(axis=0) + 0.0001)
+        
+        return self._windowing(dataset)
+            
+    def _windowing(self, array):
+        X = []
+        y = []        
+        for i in range(len(array)-self.window_size*2 + 1):
+            X.append(torch.tensor(array[i:i+self.window_size]))
+            y.append(torch.tensor(array[i+self.window_size:i+self.window_size*2]))
+        
+        return X, y
+    
+    def __len__(self):
+        return len(self.inputs)
+    
+    def __getitem__(self, idx):
+        return self.inputs[idx], self.targets[idx]
