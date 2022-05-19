61d245b0e4e8377e51e53a200c70388316b4c976
onebottlekick <gksqudcks97@gmail.com>
2022-05-19 17:27:10 +0900

    updated

--- CNN/gan/utils.py
+++ CNN/gan/utils.py
@@ -1,18 +1,19 @@
+import datetime
 import glob
 import os
-import datetime
 
-import matplotlib.pyplot as plt
-from IPython import display
 import imageio
+import matplotlib.pyplot as plt
 import torch
+from IPython import display
+from PIL import Image
 from torchvision.utils import make_grid, save_image
 
 
 def train(generator, discriminator, dataloader, g_optimizer, d_optimizer, criterion, device):
     g_losses = 0
     d_losses = 0
-    for img, _ in dataloader:
+    for idx, (img, _) in enumerate(dataloader):
         real = img.to(device)
         
         valid = torch.ones(img.size(0), 1, requires_grad=False, device=device)
@@ -24,6 +25,9 @@ def train(generator, discriminator, dataloader, g_optimizer, d_optimizer, criter
         g_loss = criterion(discriminator(gen), valid)
         g_loss.backward()
         g_optimizer.step()
+        
+        if idx % 30 == 0:
+            plot(generator, device)
         
         d_optimizer.zero_grad()
         real_loss = criterion(discriminator(real), valid)
@@ -38,17 +42,16 @@ def train(generator, discriminator, dataloader, g_optimizer, d_optimizer, criter
     return g_losses/len(dataloader), d_losses/len(dataloader)
 
 
-def plot(generator, device, n_imgs=100, save_imgs=True):
+def plot(generator, device, n_imgs=100, save_imgs=True, save_path='results', show_imgs=False):
     z = torch.randn(n_imgs, 100, device=device)
     gen_imgs = generator(z)    
     
     if save_imgs:
         grid = make_grid(gen_imgs, nrow=10, pad_value=255)
-        save_path = 'results'
         os.makedirs(save_path, exist_ok=True)
         save_image(grid, os.path.join(save_path, f'{datetime.datetime.now().strftime("%Y-%m-%d-%H_%M_%S")}.png'))
     
-    else:
+    if show_imgs:
         grid = make_grid(gen_imgs, nrow=10).permute(1, 2, 0).detach().cpu()
         with plt.ion():
             display.clear_output()
@@ -61,6 +64,12 @@ def plot(generator, device, n_imgs=100, save_imgs=True):
             plt.pause(0.1)
             
 
-def make_gif(root='results'):
-    imgs = [imageio.imread(img) for img in glob.glob(os.path.join(root, '*.png'))]
-    imageio.mimsave('mnist.gif', imgs, duration=0.5)
+def img2gif(root='results', to='result.gif', remove_imgs=False, duration=0.5):
+    imgs = [Image.open(os.path.join(root, img)) for img in sorted(os.listdir(root)) if img.endswith('png')]    
+    imageio.mimsave(os.path.join(root, to), imgs, duration=duration)
+    
+    if remove_imgs:
+        del imgs
+        for img in os.listdir(root):
+            if not img.endswith(".gif"):
+                os.remove(os.path.join(root, img))
